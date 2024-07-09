0cf6ebd832ee0775865bf1482da185575015cb28
Wilfried M Majaliwa <wilfriedmajaliwa@gmail.com>
2024-07-09 17:21:03 +0300

    Build an SPA

--- routes/index.js
+++ routes/index.js
@@ -1,10 +1,35 @@
 import express from 'express';
+import data from '../data/articles.js';
 
 const router = express.Router();
 
-router.get('/', (req, res) => {
-  res.render('index', {title: 'Product Listing'});
+router.get('/', (_, res) => {
+  res.render('index', {
+    title: 'Product Listing',
+    articles: data
+  });
 });
+
+router.get('/articles/:id', (req, res) => {
+  const id = parseInt(req.params.id);
+  const article = data.find(a => a.id === id);
+  res.render('article', {
+    title: article.name,
+    article: article.body
+  });
+});
+
+router.post('/articles', (req, res) => {
+  const { name, body } = req.body;
+  data.push({
+    id: Math.floor(Math.random() * 1000000),
+    name,
+    body
+  })
+  setTimeout(() => {
+    res.render('partials/list', {articles: data});
+  }, 3000);
+})
 
 export default router;
 
